98c24e3218c309cf6eaa2e40d44d902b70310b9a
sunnygarasia <sgarasia11@gmail.com>
2022-07-19 00:10:13 -0400

    feat: created contact and signup

--- myapp/models.py
+++ myapp/models.py
@@ -3,23 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-# class Contact(models.Model):
-#   name = models.CharField(max_length=100)
-#  email = models.CharField(max_length=100)
-# mobile = models.IntegerField()
-#add_info = models.TextField()
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=10)
+    addinfo = models.TextField()
 
-# def __str__(self):
-#   return self.name
+    def __str__(self):
+        return self.name
 
 
-# class User(models.Model):
-#  fname = models.CharField(max_length=100)
-#  lname = models.CharField(max_length=100)
-#  email = models.CharField(max_length=100)
-#  mobile = models.IntegerField()
-# address = models.TextField()
-# password = models.CharField(max_length=50)
+class User(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    cpassword = models.CharField(max_length=50)
 
-# def __str__(self):
-#     return self.fname + " - " + self.lname
+    def __str__(self):
+        return self.fname + " - " + self.lname
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,8 @@
+
+import email
+import re
 from django.shortcuts import render
+from .models import Contact, User
 
 
 # Create your views here.
@@ -17,8 +21,41 @@ def login(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == "POST":
+        try:
+            User.objects.get(email=request.POST['email'])
+            msg = "Email already registered."
+            return render(request, 'signup.html', {"msg": msg})
+        except:
+            if request.POST['password'] == request.POST['cpassword']:
+                User.objects.create(
+                    fname=request.POST['fname'],
+                    lname=request.POST['lname'],
+                    email=request.POST['email'],
+                    password=request.POST['password'],
+                    cpassword=request.POST['cpassword'],
+
+
+                )
+                msg = "Signup Successfull"
+                return render(request, 'login.html', {'msg': msg})
+            else:
+                msg = "Password doesnot match"
+                return render(request, 'signup.html', {'msg': msg})
+    else:
+        return render(request, 'signup.html')
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    if request.method == "POST":
+        Contact.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            mobile=request.POST['mobile'],
+            addinfo=request.POST['addinfo'],
+        )
+        msg = "Request Sent Successfully"
+        return render(request, 'contact.html', {'msg': msg})
+
+    else:
+        return render(request, 'contact.html')
