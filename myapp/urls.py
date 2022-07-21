54d9c199fee4f833ab6029bd85a48be7610f26c2
sunnygarasia <sgarasia11@gmail.com>
2022-07-20 23:45:06 -0400

    feat: more templates added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,11 +5,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sneakers/', views.sneakers, name='sneakers'),
     path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('account_info/', views.account_info, name='account_info'),
     path('contact/', views.contact, name='contact'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('new_password/', views.new_password, name='new_password'),
     path('verify_otp/', views.verify_otp, name='verify_otp'),
-
 
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,8 @@
-
-import email
-import re
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Contact, User
+from django.conf import settings
+from django.core.mail import send_mail
+import random
 
 
 # Create your views here.
@@ -23,11 +23,22 @@ def login(request):
                 email=request.POST['email'],
                 password=request.POST['password']
             )
+            request.session['email'] = user.email
+            request.session['fname'] = user.fname
             return render(request, 'index.html')
         except:
             msg = "Email or password incorrect"
             return render(request, 'login.html', {'msg': msg})
     else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    try:
+        del request.session['email']
+        del request.session['fname']
+        return render(request, 'index.html')
+    except:
         return render(request, 'login.html')
 
 
@@ -58,7 +69,24 @@ def signup(request):
 
 
 def forgot_password(request):
-    return render(request, 'forgot_password.html')
+    if request.method == "POST":
+        try:
+            user = User.objects.get(email=request.POST['email'])
+            otp = random.randint(1000, 9999)
+            subject = 'One Time Password'
+            message = 'Hi '+user.fname + \
+                ', Here Is Your One Time Password :' + str(otp)
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [user.email, ]
+            send_mail(subject, message, email_from, recipient_list)
+            return render(request, 'otp.html', {'otp': otp, 'email': user.email})
+
+        except:
+            msg = "Email Not Registered Yet."
+            return render(request, 'forgot_password.html', {'msg': msg})
+
+    else:
+        return render(request, 'forgot_password.html')
 
 
 def contact(request):
@@ -89,9 +117,47 @@ def verify_otp(request):
         return render(request, 'otp.html', {'email': email, 'otp': otp, 'msg': msg})
 
 
-def new_passworf(request):
+def account_info(request):
+    user = User.objects.get(email=request.session['email'])
+    if request.method == "POST":
+        user.fname = request.POST['fname']
+        user.lname = request.POST['lname']
+        user.email = request.POST['email']
+
+        return render(request, 'account_info.html', {'user': user})
+    else:
+        return render(request, 'account_info.html', {'user': user})
+
+
+def change_password(request):
+    if request.method == "POST":
+        user = User.objects.get(email=request.session['email'])
+        if request.POST['password'] == user.password:
+            if request.POST['new_password'] == request.POST['cnew_password']:
+                user.password = request.POST['new_password']
+                user.save()
+                return redirect('logout')
+            else:
+                msg = "Both Password Doesnot Match"
+                return render(request, 'change_password.html', {'msg': msg})
+        else:
+            msg = "Old Password Doesnot Match"
+            return render(request, 'change_password.html', {'msg': msg})
+    else:
+        return render(request, 'change_password.html')
+
+
+def new_password(request):
     email = request.POST['email']
     new_password = request.POST['new_password']
     cnew_password = request.POST['cnew_password']
 
-    return render(request, 'login.html')
+    if new_password == cnew_password:
+        user = User.objects.get(email=email)
+        user.password = new_password
+        user.save()
+        msg = "Password Updated Succesfully. You May Login Now"
+        return render(request, 'login.html', {'msg': msg})
+    else:
+        msg = "Password Didn't Match"
+        return render(request, 'new_password.html', {'email': email, 'msg': msg})
